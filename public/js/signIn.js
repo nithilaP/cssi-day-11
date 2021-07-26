cd65574fbfe8619bd30088afcb9795035e1b2d1f
nithilap <nithila@poongo.net>
2021-07-26 17:43:56 +0000

    Redirect after Log in Page.

---
+++ public/js/signIn.js
@@ -0,0 +1,18 @@
+const signInButton = document.querySelector(".button");
+console.log("Sign in running")
+
+signInButton.addEventListener("click", () => {
+    console.log("clicked");
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+
+
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+    .catch(error => {
+        console.log("login failed", error)
+    })
+
+})
